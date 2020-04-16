d9b5d7f7cc484cb80c92befd166c87e867987a70
Fabio Cortes <fcor14@hotmail.com>
2020-04-16 18:28:14 -0500

    Dual bridge!

--- modules/acids-and-bases/hbonds-htransfer/index.js
+++ modules/acids-and-bases/hbonds-htransfer/index.js
@@ -24,8 +24,8 @@ AFRAME.registerComponent("update-stuff", {
     this.glu11B = this.el.sceneEl.querySelector("#glu11B");
     this.glu12B = this.el.sceneEl.querySelector("#glu12B");
 
-    // this.bridge = this.el.sceneEl.querySelector("#bridge");
-    // this.connectors = this.bridge.querySelectorAll("*");
+    this.bridge = this.el.sceneEl.querySelector("#bridge");
+    this.connectors = this.bridge.querySelectorAll("*");
 
     // Create Position vectors for further usage
     this.lys6APosition = new THREE.Vector3();
@@ -90,6 +90,7 @@ AFRAME.registerComponent("update-stuff", {
     const areEnoughMarkers = this.visibleMarkers.length === 2;
 
     if (areMarkersInvalid || !areEnoughMarkers) {
+      this.bridge.setAttribute("visible", false);
       return;
     }
 
@@ -100,80 +101,103 @@ AFRAME.registerComponent("update-stuff", {
     this.lys15APosition.setFromMatrixPosition(this.lys15A.object3D.matrixWorld);
     this.lys16APosition.setFromMatrixPosition(this.lys16A.object3D.matrixWorld);
     this.lys17APosition.setFromMatrixPosition(this.lys17A.object3D.matrixWorld);
-    this.glu5APosition.setFromMatrixPosition(this.glu5A.object3D.matrixWorld);
-    this.glu6APosition.setFromMatrixPosition(this.glu6A.object3D.matrixWorld);
-    this.glu11APosition.setFromMatrixPosition(this.glu11A.object3D.matrixWorld);
-    this.glu12APosition.setFromMatrixPosition(this.glu12A.object3D.matrixWorld);
+
     this.lys6BPosition.setFromMatrixPosition(this.lys6B.object3D.matrixWorld);
     this.lys15BPosition.setFromMatrixPosition(this.lys15B.object3D.matrixWorld);
     this.lys16BPosition.setFromMatrixPosition(this.lys16B.object3D.matrixWorld);
     this.lys17BPosition.setFromMatrixPosition(this.lys17B.object3D.matrixWorld);
+
+    this.glu5APosition.setFromMatrixPosition(this.glu5A.object3D.matrixWorld);
+    this.glu6APosition.setFromMatrixPosition(this.glu6A.object3D.matrixWorld);
+    this.glu11APosition.setFromMatrixPosition(this.glu11A.object3D.matrixWorld);
+    this.glu12APosition.setFromMatrixPosition(this.glu12A.object3D.matrixWorld);
+
     this.glu5BPosition.setFromMatrixPosition(this.glu5B.object3D.matrixWorld);
     this.glu6BPosition.setFromMatrixPosition(this.glu6B.object3D.matrixWorld);
     this.glu11BPosition.setFromMatrixPosition(this.glu11B.object3D.matrixWorld);
     this.glu12BPosition.setFromMatrixPosition(this.glu12B.object3D.matrixWorld);
 
     // Storing a reference of all elements in arrays for easier handling
-    const lysine = [this.lys15, this.lys16, this.lys17];
-    const glutamate = [this.glu11, this.glu12];
+    const lysineA = [this.lys15A, this.lys16A, this.lys17A];
+    const lysineB = [this.lys15B, this.lys16B, this.lys17B];
+
+    const glutamateA = [this.glu11A, this.glu12A];
+    const glutamateB = [this.glu11B, this.glu12B];
 
     // Storing a reference of all positions in arrays for easier handling
-    const lysinePositions = [
-      this.lys15Position,
-      this.lys16Position,
-      this.lys17Position,
+    const lysineAPositions = [
+      this.lys15APosition,
+      this.lys16APosition,
+      this.lys17APosition,
     ];
-    const glutamatePositions = [this.glu11Position, this.glu12Position];
+    const lysineBPositions = [
+      this.lys15BPosition,
+      this.lys16BPosition,
+      this.lys17BPosition,
+    ];
 
-    // let closestDistance = 10000000;
-    // let closestLysine;
-    // let closestGlutamate;
+    const glutamateAPositions = [this.glu11APosition, this.glu12APosition];
+    const glutamateBPositions = [this.glu11BPosition, this.glu12BPosition];
 
-    // // Calc distances
-    // lysinePositions.forEach((lysineItem, lysineIndex) => {
-    //   glutamatePositions.forEach((glutamateItem, glutamateIndex) => {
-    //     const distance = 2 * Math.sqrt(Math.pow(glutamateItem.x-lysineItem.x,2) + Math.pow(glutamateItem.y-lysineItem.y,2) + Math.pow(glutamateItem.z-lysineItem.z,2));
-    //     if(distance < closestDistance) {
-    //       closestDistance = distance;
-    //       closestLysine = lysineIndex;
-    //       closestGlutamate = glutamateIndex;
-    //     }
-    //   });
-    // });
+    // Which side of each molecule is visible? Select corresponding positions & elements
+    const lysineSide = this.visibleMarkers.includes("lysA") ? "A" : "B";
+    const glutamateSide = this.visibleMarkers.includes("gluA") ? "A" : "B";
+
+    const lysinePositions = lysineSide === "A" ? lysineAPositions : lysineBPositions;
+    const glutamatePositions = glutamateSide === "A" ? glutamateAPositions : glutamateBPositions;
+
+    const lysine = lysineSide === "A" ? lysineA : lysineB;
+    const glutamate = glutamateSide === "A" ? glutamateA : glutamateB;
+
+    // Calc distances
+    let closestDistance = 10000000;
+    let closestLysine;
+    let closestGlutamate;
+
+    lysinePositions.forEach((lysineItem, lysineIndex) => {
+      glutamatePositions.forEach((glutamateItem, glutamateIndex) => {
+        const distance = 2 * Math.sqrt(Math.pow(glutamateItem.x-lysineItem.x,2) + Math.pow(glutamateItem.y-lysineItem.y,2) + Math.pow(glutamateItem.z-lysineItem.z,2));
+        if(distance < closestDistance) {
+          closestDistance = distance;
+          closestLysine = lysineIndex;
+          closestGlutamate = glutamateIndex;
+        }
+      });
+    });
 
     // What shoud be bridge's direction?
-    //   if (this.lys_prot == 1) {
-    //     [...this.connectors].forEach((connector, index) => {
-    //       // If closestGlutamate is glu11, its corrsponding oxygen is glu6
-    //       // If closestGlutamate is glu12, its corrsponding oxygen is glu5
-    //       const correspondingOxygen = closestGlutamate === 0 ? "glu6" : "glu5";
-    //       connector.setAttribute(
-    //         "connector",
-    //         `src: #lys${closestLysine + 15}; dest: #${correspondingOxygen}; alpha: ` + index / 10,
-    //       );
-    //     });
-    //   } else if (this.glu_prot == 1) {
-    //     [...this.connectors].forEach((connector, index) => {
-    //       connector.setAttribute(
-    //         "connector",
-    //         "src: #lys6; dest: #glu11; alpha: " + index / 10
-    //       );
-    //     });
-    //   } else if (this.glu_prot == 2) {
-    //     [...this.connectors].forEach((connector, index) => {
-    //       connector.setAttribute(
-    //         "connector",
-    //         "src: #lys6; dest: #glu12; alpha: " + index / 10
-    //       );
-    //     });
-    //   }
+    if (this.lys_prot == 1) {
+      [...this.connectors].forEach((connector, index) => {
+        // If closestGlutamate is glu11, its corrsponding oxygen is glu6
+        // If closestGlutamate is glu12, its corrsponding oxygen is glu5
+        const correspondingOxygen = closestGlutamate === 0 ? "glu6" : "glu5";
+        connector.setAttribute(
+          "connector",
+          `src: #lys${closestLysine + 15}${lysineSide}; dest: #${correspondingOxygen}${glutamateSide}; alpha: ` + index / 10,
+        );
+      });
+    } else if (this.glu_prot == 1) {
+      [...this.connectors].forEach((connector, index) => {
+        connector.setAttribute(
+          "connector",
+          `src: #lys6${lysineSide}; dest: #glu11${glutamateSide}; alpha: ` + index / 10
+        );
+      });
+    } else if (this.glu_prot == 2) {
+      [...this.connectors].forEach((connector, index) => {
+        connector.setAttribute(
+          "connector",
+          `src: #lys6${lysineSide}; dest: #glu12${glutamateSide}; alpha: ` + index / 10
+        );
+      });
+    }
 
-    //   // Which element are closer? should we display bridge?
-    //   const isClosestLysineVisible = lysine[closestLysine].getAttribute('visible');
-    //   const isClosestGlutamateVisible = glutamate[closestGlutamate].getAttribute('visible');
+      // Which element are closer? should we display bridge?
+      const isClosestLysineVisible = lysine[closestLysine].getAttribute('visible');
+      const isClosestGlutamateVisible = glutamate[closestGlutamate].getAttribute('visible');
 
-    //   if(closestDistance < 2) {
-    //     this.bridge.setAttribute("visible", true);
+      if(closestDistance < 2) {
+        this.bridge.setAttribute("visible", true);
     //     if (closestGlutamate === 0 && (this.glu_prot == 0 || this.glu_prot == 1)) {
     //       if (Math.random() < 0.2) {
     //         if(isClosestGlutamateVisible && !isClosestLysineVisible && this.lys_prot === 0) {
@@ -207,8 +231,8 @@ AFRAME.registerComponent("update-stuff", {
     //         }
     //       }
     //     }
-    //   } else {
-    //     this.bridge.setAttribute("visible", false);
-    //   }
+      } else {
+        this.bridge.setAttribute("visible", false);
+      }
   },
 });
