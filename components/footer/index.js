a7f04c47516c67fa93e019308289f9c9278cae09
Fabio Cortes <fcor14@hotmail.com>
2020-04-29 09:54:18 -0500

    Fixed footer and side menu on mobile :)

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,4 @@
-const AppFooterContent = `
+const AppFooterContent = /* html */ `
   <style>
     footer {
       height: 15rem;
@@ -73,6 +73,144 @@ const AppFooterContent = `
       bottom: 0.1rem;
       left: 50%;
       transform: translate(-50%, 0);
+      width: 100%;
+      text-align: center;
+    }
+
+    @media screen and (max-width: 1200px) {
+      .contact-info {
+        padding-left: 2rem;
+      }
+    }
+
+    @media screen and (max-width: 1050px) {
+      .footer-top {
+        justify-content: space-between;
+      }
+      .logos {
+        justify-content: space-between;
+      }
+    }
+    @media screen and (max-width: 1000px) {
+      .contact-info {
+        padding-left: 1.25rem;
+      }
+    }
+    @media screen and (max-width: 900px) {
+      footer {
+        height: 13rem;
+        justify-content: flex-start;
+      }
+      .footer-top {
+        margin-top: 1rem
+      }
+
+      .contact-info {
+        padding-left: 2.5rem;
+      }
+
+      .contact-us {
+        font-size: 18px;
+        margin: 1rem 0;
+      }
+
+      .contact-name {
+        font-size: 14px;
+      }
+
+      .contact-email {
+        font-size: 12px;
+      }
+
+      .logos {
+        justify-content: space-between;
+        margin-right: 2.5rem
+      }
+
+      img {
+        height: 30px;
+      }
+    }
+    @media screen and (max-width: 900px) {
+      .logos {
+        margin-right: 1rem
+      }
+      .contact-info {
+        padding-left: 1rem;
+      }
+    }
+    @media screen and (max-width: 750px) {
+      .logos {
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 1rem
+      }
+      .logos a:nth-of-type(2n) {
+        margin: 1rem 0
+      }
+      .contact-info {
+        border: none;
+      }
+      .privacy-policy {
+        font-size: 10px;
+      }
+    }
+
+    @media screen and (max-width: 500px) {
+      .privacy-policy {
+        font-size: 8px;
+      }
+    }
+
+    @media screen and (max-width: 500px) {
+      footer {
+        height: 11.5rem;
+      }
+
+      img {
+        height: 25px;
+      }
+      .contact-us {
+        font-size: 16px;
+        margin: 0.8rem 0;
+      }
+
+      .contact-name {
+        font-size: 12px;
+      }
+
+      .contact-email {
+        font-size: 10px;
+      }
+    }
+    @media screen and (max-width: 450px) {
+      .logos {
+        margin-left: 0;
+      }
+
+      .contact-info {
+        padding-left: 0.5rem;
+      }
+    }
+    @media screen and (max-width: 350px) {
+      img {
+        height: 25px;
+      }
+      .privacy-policy {
+        font-size: 7px;
+      }
+      .contact-us {
+        font-size: 14px;
+        margin: 0.8rem 0;
+      }
+
+      .contact-name {
+        font-size: 10px;
+      }
+
+      .contact-email {
+        font-size: 8px;
+      }
     }
   </style>
   <footer>
