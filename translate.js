e58c21d61221eacda267b49f3018835582a5ef4f
Fabio Cortes <fcor14@hotmail.com>
2020-05-01 10:32:13 -0500

    moved all eng texts to translation files

--- translate.js
+++ translate.js
@@ -24,7 +24,7 @@ i18next
     function (err, t) {
       localize = locI18next.init(i18next);
 
-      localize(".menu");
+      localize(".content");
 
       console.log(
         `detected user language: "${
