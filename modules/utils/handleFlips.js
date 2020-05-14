5b5752fad558e75caa80665ca61ba2a1d0a9b9a2
Fabio Cortes <fcor14@hotmail.com>
2020-05-14 15:37:19 -0500

    Fixed canvas rotation and added a nice icon :)

--- modules/utils/handleFlips.js
+++ modules/utils/handleFlips.js
@@ -4,6 +4,7 @@ const flipVideoButton = document.querySelector("flip-video");
 function handleCameraFlip() {
   const video = document.getElementsByTagName("video")[0];
   video.classList.toggle("flip");
+  canvas.classList.toggle("flip");
 }
 
 flipVideoButton.addEventListener("flipCamera", handleCameraFlip);
