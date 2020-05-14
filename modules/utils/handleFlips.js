93ebbbf1c5ea984919cca50e501a19cbc8afd8f7
Fabio Cortes <fcor14@hotmail.com>
2020-05-14 14:39:03 -0500

    Added basic video flip component

---
+++ modules/utils/handleFlips.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas");
+const flipVideoButton = document.querySelector("flip-video");
+
+function handleCameraFlip() {
+  const video = document.getElementsByTagName("video")[0];
+  video.classList.toggle("flip");
+}
+
+flipVideoButton.addEventListener("flipCamera", handleCameraFlip);
