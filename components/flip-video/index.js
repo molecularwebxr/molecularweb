75975def5e144f97536b31936ad9011ee0c8f4f5
Fabio Cortes <fcor14@hotmail.com>
2020-06-05 09:43:35 -0500

    Updated icon size for mobile

--- components/flip-video/index.js
+++ components/flip-video/index.js
@@ -18,6 +18,12 @@ const FlipVideoContent = /* html */ `
     a:hover #circle{
       fill: var(--secondary);
     }
+    @media (max-width: 440px) {
+      svg {
+        width: 30px;
+        height: 30px;
+      }
+    }
   </style>
   <a id="anchor">
     <svg width="100%" height="100%" viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
