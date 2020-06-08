6d6cfd3209e638643aca2a4646b22ecff576ed7d
Fabio Cortes <fcor14@hotmail.com>
2020-06-08 10:47:41 -0500

    Extracted rotation controls into a component and added custom range styles

---
+++ components/rotation-controls/index.js
@@ -0,0 +1,219 @@
+const RotationControlsContent = document.createElement("template");
+
+RotationControlsContent.innerHTML = /* html */ `
+  <style>
+  input[type=range] {
+    -webkit-appearance: none;
+    margin: 10px auto;
+    width: 90px;
+    background-color: transparent;
+    cursor: pointer;
+  }
+  input[type=range]:focus {
+    outline: none;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    height: 3px;
+    width: 90px;
+    border-radius: 2px;
+    cursor: pointer;
+    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
+    background: #000000;
+    border-radius: 25px;
+    border: 0px solid #000000;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    border: 0px solid #000000;
+    width: 12px;
+    height: 12px;
+    background-color: white;
+    border-radius: 6px;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -3.6px;
+  }
+  input[type=range]:focus::-webkit-slider-runnable-track {
+    background: #000000;
+  }
+  input[type=range]::-moz-range-track {
+    height: 3px;
+    width: 90px;
+    border-radius: 2px;
+    cursor: pointer;
+    animate: 0.2s;
+    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
+    background: #000000;
+    border: 0px solid #000101;
+  }
+  input[type=range]::-moz-range-thumb {
+    border: 0px solid #000000;
+    width: 12px;
+    height: 12px;
+    background-color: white;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+  input[type=range]::-ms-track {
+    height: 3px;
+    width: 90px;
+    border-radius: 2px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: transparent;
+    border-color: transparent;
+    border-width: 39px 0;
+    color: transparent;
+  }
+  input[type=range]::-ms-fill-lower {
+    background: #000000;
+    border: 0px solid #000101;
+    border-radius: 50px;
+    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
+  }
+  input[type=range]::-ms-fill-upper {
+    background: #000000;
+    border: 0px solid #000101;
+    border-radius: 50px;
+    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
+  }
+  input[type=range]::-ms-thumb {
+    border: 0px solid #000000;
+    width: 12px;
+    height: 12px;
+    background-color: white;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+  input[type=range]:focus::-ms-fill-lower {
+    background: #000000;
+  }
+  input[type=range]:focus::-ms-fill-upper {
+    background: #000000;
+  }
+
+  .rotation-controls {
+    position: fixed;
+    bottom: 64px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+    width: 540px;
+    display: flex;
+  }
+
+  .rotation-marker1{
+    width: 241px;
+    padding:  0 12px;
+    box-sizing: border-box;
+  }
+
+  .rotation-bar {
+    width: 180px;
+    height: 20px;
+    background-color: rgba(91, 69, 235, 0.8);
+    margin: 4px 0;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px 0 8px;
+    
+  }
+
+  .slider {
+    height: 3px;
+    width: 90px;
+    border-radius: 2px;
+    margin: 8px;
+    background-color: black;
+    position: relative;
+  }
+
+  .handler{
+    width: 12px;
+    height: 12px;
+    background-color: white;
+    border-radius: 6px;
+    position: absolute;
+    top: -5px;
+  }
+
+  .number-box{
+    width: 26px;
+  }
+
+  p.small {
+    margin: 0;
+    font-size: 12px;
+    color: white;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  p.small.num {
+    text-align: right;
+  }
+  </style>
+  <div class="rotation-controls">
+    <div class="rotation-marker1">
+      <div class="rotation-bar">
+        <p class="small">Rotate Y</p>
+        <input type="range" />
+        <div class="number-box">
+          <p class="small num">0°</p>
+        </div>
+      </div>
+      <div class="rotation-bar">
+        <p class="small">Rotate X</p>
+        <input type="range" />
+        <div class="number-box">
+          <p class="small num">180°</p>
+        </div>
+      </div>
+    </div>
+    <div class="rotation-marker1">
+      <div class="rotation-bar">
+        <p class="small">Rotate Y</p>
+        <input type="range" />
+        <div class="number-box">
+          <p class="small num">0°</p>
+        </div>
+      </div>
+      <div class="rotation-bar">
+        <p class="small">Rotate X</p>
+        <input type="range" />
+        <div class="number-box">
+          <p class="small num">180°</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+class RotationControls extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(RotationControlsContent.content.cloneNode(true));
+
+  }
+
+  static get observedAttributes() {
+    return ["display"];
+  }
+
+  set display(value) {
+    this._display = value;
+  }
+
+  get display() {
+    return this._display;
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName === "display") {
+      this.display = newValue;
+    }
+  }
+
+}
+
+customElements.define("rotation-controls", RotationControls);
