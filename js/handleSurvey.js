1f71af68d268157eeb5fb3988986ca6864551c20
Fabio Cortes <fcor14@hotmail.com>
2020-06-29 15:01:10 -0500

    Displaying questions according to user's profile

--- js/handleSurvey.js
+++ js/handleSurvey.js
@@ -1,4 +1,16 @@
 var form = document.getElementById("survey-form");
+var educatorsProfile = document.getElementById("profile-option-1");
+var studentsProfile = document.getElementById("profile-option-2");
+var educatorQuestions = document.querySelectorAll(".educators");
+var studentQuestions = document.querySelectorAll(".students");
+
+educatorQuestions.forEach(function(item) {
+  item.classList.add("hidden");
+});
+
+studentQuestions.forEach(function(item) {
+  item.classList.remove("hidden");
+});
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -7,4 +19,26 @@ function handleSubmit(e) {
   console.log(formData.profile.value);
 }
 
+function handleProfileChange(event) {
+  if(event.target.value === "educator"){
+    educatorQuestions.forEach(function(item) {
+      item.classList.remove("hidden");
+    });
+
+    studentQuestions.forEach(function(item) {
+      item.classList.add("hidden");
+    });
+  } else {
+    educatorQuestions.forEach(function(item) {
+      item.classList.add("hidden");
+    });
+
+    studentQuestions.forEach(function(item) {
+      item.classList.remove("hidden");
+    });
+  }
+}
+
 form.addEventListener("submit", handleSubmit);
+educatorsProfile.addEventListener("change", handleProfileChange);
+studentsProfile.addEventListener("change", handleProfileChange);
