7f777d930aafc4805cc2af7261d8905a046e7187
Fabio Cortes <fcor14@hotmail.com>
2020-07-01 17:14:04 -0500

    Added interaction!

--- modules/atomic-structures/protein-dna/index.js
+++ modules/atomic-structures/protein-dna/index.js
@@ -1,19 +1,19 @@
 /* global AFRAME, THREE */
 
-AFRAME.registerComponent('connector', {
+AFRAME.registerComponent("connector", {
   schema: {
-    src:  {type: 'selector'},
-    dest: {type: 'selector'},
-    alpha: {type: 'number', default: 0.5}
+    src: { type: "selector" },
+    dest: { type: "selector" },
+    alpha: { type: "number", default: 0.5 },
   },
 
-  init: function() {
+  init: function () {
     this.p0 = new THREE.Vector3();
     this.p1 = new THREE.Vector3();
     this.pf = new THREE.Vector3();
   },
 
-  tick: function() {
+  tick: function () {
     var obj0 = this.data.src.object3D;
     var obj1 = this.data.dest.object3D;
     this.p0.setFromMatrixPosition(obj0.matrixWorld);
@@ -22,17 +22,17 @@ AFRAME.registerComponent('connector', {
     this.el.setAttribute("position", this.pf);
 
     var dist = norm(this.p0.toArray(), this.p1.toArray(), 3);
-    var opacity = 1.0 - Math.max(Math.min((dist - 1.0)/(3.0 - 1.0), 1.0), 0.0);
+    var opacity =
+      1.0 - Math.max(Math.min((dist - 1.0) / (3.0 - 1.0), 1.0), 0.0);
 
     this.el.setAttribute("opacity", opacity);
 
     var color = interpolateLinearly(opacity, winter);
-    var r = Math.floor(255*color[0]);
-    var g = Math.floor(255*color[1]);
-    var b = Math.floor(255*color[2]);
+    var r = Math.floor(255 * color[0]);
+    var g = Math.floor(255 * color[1]);
+    var b = Math.floor(255 * color[2]);
 
-    this.el.setAttribute("color", "rgb("+r+","+g+","+b+")");
-
+    this.el.setAttribute("color", "rgb(" + r + "," + g + "," + b + ")");
   },
 });
 
@@ -40,28 +40,59 @@ AFRAME.registerComponent("interactive-molecules", {
   init: function () {
     // Get elements from the scene
 
-    // Water - Side A & Side B
-    this.wat1A = this.el.sceneEl.querySelector("#wat1A");
-    this.wat1B = this.el.sceneEl.querySelector("#wat1B");
+    // Prot - Side A & Side B
+    this.protA = this.el.sceneEl.querySelector("#protA");
+    this.protB = this.el.sceneEl.querySelector("#protB");
 
-    // Second water (vat) - Side A & Side B
-    this.vat1A = this.el.sceneEl.querySelector("#vat1A");
-    this.vat1B = this.el.sceneEl.querySelector("#vat1B");
+    // DNA - Side A & Side B
+    this.dnaA = this.el.sceneEl.querySelector("#dnaA");
+    this.dnaB = this.el.sceneEl.querySelector("#dnaB");
 
-    // Bridge
-    this.bridge = this.el.sceneEl.querySelector("#bridge");
-    this.connectors = this.bridge.querySelectorAll("*");
+    // Prot-DNA - Side A & Side B
+    this.protDnaA = this.el.sceneEl.querySelector("#prot-dnaA");
+    this.protDnaB = this.el.sceneEl.querySelector("#prot-dnaB");
 
-    // Create Position vectors for further use
-    this.wat1APosition = new THREE.Vector3();
-    this.wat1BPosition = new THREE.Vector3();
+    // Spheres - Side A & Side B
+    this.sp0A = this.el.sceneEl.querySelector("#sp0A");
+    this.sp1A = this.el.sceneEl.querySelector("#sp1A");
+    this.sp2A = this.el.sceneEl.querySelector("#sp2A");
+    this.sd0A = this.el.sceneEl.querySelector("#sd0A");
+    this.sd1A = this.el.sceneEl.querySelector("#sd1A");
+    this.sd2A = this.el.sceneEl.querySelector("#sd2A");
+    this.sp0B = this.el.sceneEl.querySelector("#sp0B");
+    this.sp1B = this.el.sceneEl.querySelector("#sp1B");
+    this.sp2B = this.el.sceneEl.querySelector("#sp2B");
+    this.sd0B = this.el.sceneEl.querySelector("#sd0B");
+    this.sd1B = this.el.sceneEl.querySelector("#sd1B");
+    this.sd2B = this.el.sceneEl.querySelector("#sd2B");
 
-    this.vat1APosition = new THREE.Vector3();
-    this.vat1BPosition = new THREE.Vector3();
+    // Bridges & Connectors
+    this.bridge1 = this.el.sceneEl.querySelector("#bridge-1");
+    this.connectors1 = this.bridge1.querySelectorAll("*");
+    this.bridge2 = this.el.sceneEl.querySelector("#bridge-2");
+    this.connectors2 = this.bridge2.querySelectorAll("*");
+    this.bridge3 = this.el.sceneEl.querySelector("#bridge-3");
+    this.connectors3 = this.bridge3.querySelectorAll("*");
 
-    // Not used in this sample. This vars are here as reminder of placing them here.
-    this.protoWat = 2;
-    this.protoVat = 2;
+    // Create Position vectors for further usage
+    this.p_sp0A = new THREE.Vector3();
+    this.p_sp1A = new THREE.Vector3();
+    this.p_sp2A = new THREE.Vector3();
+    this.p_sd0A = new THREE.Vector3();
+    this.p_sd1A = new THREE.Vector3();
+    this.p_sd2A = new THREE.Vector3();
+    this.p_sp0B = new THREE.Vector3();
+    this.p_sp1B = new THREE.Vector3();
+    this.p_sp2B = new THREE.Vector3();
+    this.p_sd0B = new THREE.Vector3();
+    this.p_sd1B = new THREE.Vector3();
+    this.p_sd2B = new THREE.Vector3();
+
+    // tolerance parameter
+    this.tol_d = 1.3;
+
+    // Are objects docked?
+    this.docked = false;
 
     // Interval for running tick function - in ms
     this.interval = 200;
@@ -101,96 +132,88 @@ AFRAME.registerComponent("interactive-molecules", {
     }
     this.time = t;
 
+    // Are objects docked? return if so
+    if (this.docked) {
+      return;
+    }
+
     // Don't do anything if find wrong marker setup or if there are not enough markers
     // e. g. 3 or more markers at the same time or both sides of a single molecule
     const areMarkersInvalid =
-      (this.visibleMarkers.includes("watA") &&
-        this.visibleMarkers.includes("watB")) ||
-      (this.visibleMarkers.includes("vatA") &&
-        this.visibleMarkers.includes("vatB"));
+      (this.visibleMarkers.includes("prot-A") &&
+        this.visibleMarkers.includes("prot-B")) ||
+      (this.visibleMarkers.includes("dna-A") &&
+        this.visibleMarkers.includes("dna-B"));
 
     const areEnoughMarkers = this.visibleMarkers.length === 2;
 
     if (areMarkersInvalid || !areEnoughMarkers) {
-      this.bridge.setAttribute("visible", false);
+      this.bridge1.setAttribute("visible", false);
+      this.bridge2.setAttribute("visible", false);
+      this.bridge3.setAttribute("visible", false);
       return;
     }
 
     this.el.sceneEl.object3D.updateMatrixWorld();
 
     //  Get positions
-    this.wat1APosition.setFromMatrixPosition(this.wat1A.object3D.matrixWorld);
-    this.wat1BPosition.setFromMatrixPosition(this.wat1B.object3D.matrixWorld);
-    this.vat1APosition.setFromMatrixPosition(this.vat1A.object3D.matrixWorld);
-    this.vat1BPosition.setFromMatrixPosition(this.vat1B.object3D.matrixWorld);
+    this.p_sp0A.setFromMatrixPosition(this.sp0A.object3D.matrixWorld);
+    this.p_sp1A.setFromMatrixPosition(this.sp1A.object3D.matrixWorld);
+    this.p_sp2A.setFromMatrixPosition(this.sp2A.object3D.matrixWorld);
+    this.p_sd0A.setFromMatrixPosition(this.sd0A.object3D.matrixWorld);
+    this.p_sd1A.setFromMatrixPosition(this.sd1A.object3D.matrixWorld);
+    this.p_sd2A.setFromMatrixPosition(this.sd2A.object3D.matrixWorld);
+    this.p_sp0B.setFromMatrixPosition(this.sp0B.object3D.matrixWorld);
+    this.p_sp1B.setFromMatrixPosition(this.sp1B.object3D.matrixWorld);
+    this.p_sp2B.setFromMatrixPosition(this.sp2B.object3D.matrixWorld);
+    this.p_sd0B.setFromMatrixPosition(this.sd0B.object3D.matrixWorld);
+    this.p_sd1B.setFromMatrixPosition(this.sd1B.object3D.matrixWorld);
+    this.p_sd2B.setFromMatrixPosition(this.sd2B.object3D.matrixWorld);
 
     // Store a reference of all elements in arrays for easier handling
-    const waterA = [this.wat1A];
-    const waterB = [this.wat1B];
+    var protA = [this.p_sp0A, this.p_sp1A, this.p_sp2A];
+    var protB = [this.p_sp0B, this.p_sp1B, this.p_sp2B];
 
-    const vaterA = [this.vat1A];
-    const vaterB = [this.vat1B];
+    var dnaA = [this.p_sd0A, this.p_sd1A, this.p_sd2A];
+    var dnaB = [this.p_sd0B, this.p_sd1B, this.p_sd2B];
 
-    // Store a reference of all positions in arrays for easier handling
-    const waterAPositions = [this.wat1APosition];
-    const waterBPositions = [this.wat1BPosition];
-    const vaterAPositions = [this.vat1APosition];
-    const vaterBPositions = [this.vat1BPosition];
+    // Which side of each molecule is visible? Select corresponding positions
+    var protSide = this.visibleMarkers.includes("prot-A") ? "A" : "B";
+    var dnaSide = this.visibleMarkers.includes("dna-A") ? "A" : "B";
 
-    // Which side of each molecule is visible? Select corresponding positions & elements
-    const waterSide = this.visibleMarkers.includes("watA") ? "A" : "B";
-    const vaterSide = this.visibleMarkers.includes("vatA") ? "A" : "B";
+    var protPositions = protSide === "A" ? protA : protB;
+    var dnaPositions = dnaSide === "A" ? dnaA : dnaB;
 
-    const waterPositions =
-      waterSide === "A" ? waterAPositions : waterBPositions;
-    const vaterPositions =
-      vaterSide === "A" ? vaterAPositions : vaterBPositions;
+    var d0 = norm(protPositions[0].toArray(), dnaPositions[0].toArray(), 3);
+    var d1 = norm(protPositions[1].toArray(), dnaPositions[1].toArray(), 3);
+    var d2 = norm(protPositions[2].toArray(), dnaPositions[2].toArray(), 3);
 
-    const water = waterSide === "A" ? waterA : waterB;
-    const vater = vaterSide === "A" ? vaterA : vaterB;
+    if (
+      d0 < this.tol_d &&
+      d1 < this.tol_d &&
+      d2 < this.tol_d &&
+      d0 + d1 + d2 > 1e-5
+    ) {
+      // hide marker objects
+      this.protA.setAttribute("visible", false);
+      this.protB.setAttribute("visible", false);
+      this.dnaA.setAttribute("visible", false);
+      this.dnaB.setAttribute("visible", false);
 
-    let closestDistance = 10000000;
-    let closestWater;
-    let closestVater;
+      // hide bonds
+      this.bridge1.setAttribute("visible", false);
+      this.bridge2.setAttribute("visible", false);
+      this.bridge3.setAttribute("visible", false);
 
-    // Calc distances
-    waterPositions.forEach((waterItem, waterIndex) => {
-      vaterPositions.forEach((vaterItem, vaterIndex) => {
-        const distance =
-          2 *
-          Math.sqrt(
-            Math.pow(vaterItem.x - waterItem.x, 2) +
-              Math.pow(vaterItem.y - waterItem.y, 2) +
-              Math.pow(vaterItem.z - waterItem.z, 2)
-          );
-        if (distance < closestDistance) {
-          closestDistance = distance;
-          closestWater = waterIndex;
-          closestVater = vaterIndex;
-        }
-      });
-    });
+      // show complete object
+      this.protDnaA.setAttribute("visible", true);
+      this.protDnaB.setAttribute("visible", true);
 
-    // Not used here but may be useful to know if the closest molecules are visible or not
-    const isClosestWaterVisible = water[closestWater].getAttribute("visible");
-    const isClosestVaterVisible = vater[closestVater].getAttribute("visible");
-
-    // Here should be the logic to apply to molecules according to its distance and visibility
-    // Just showing the bridge as example
-    // For examples of this logic take a look at acids & bases activities
-    if (closestDistance < 5.5) {
-      this.bridge.setAttribute("visible", true);
-      [...this.connectors].forEach((connector, index) => {
-        connector.setAttribute(
-          "connector",
-          `src: #vat${closestVater + 1}${vaterSide}; dest: #wat${
-            closestWater + 1
-          }${waterSide}; alpha: ` +
-            index / 10
-        );
-      });
+      this.docked = true;
     } else {
-      this.bridge.setAttribute("visible", false);
+      this.bridge1.setAttribute("visible", true);
+      this.bridge2.setAttribute("visible", true);
+      this.bridge3.setAttribute("visible", true);
     }
   },
 
