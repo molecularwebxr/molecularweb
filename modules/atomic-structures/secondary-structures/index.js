91a926ae43665b1143077a6b3e61671550f0a837
Fabio Cortes <fcor14@hotmail.com>
2020-07-06 15:33:27 -0500

    Added interactivity

--- modules/atomic-structures/secondary-structures/index.js
+++ modules/atomic-structures/secondary-structures/index.js
@@ -1,75 +1,136 @@
-// Get select elements for each marker and store them in an array for easier selection and handling
-var marker1Select = document.getElementById("select-marker-1");
-var marker2Select = document.getElementById("select-marker-2");
-var markers = [marker1Select, marker2Select];
+// Get button elements for each marker 
+var cartoonButton1 = document.getElementById("cartoon-1");
+var sticksButton1 = document.getElementById("sticks-1");
+var bondsButton1 = document.getElementById("bonds-1");
+
+var cartoonButton2 = document.getElementById("cartoon-2");
+var sticksButton2 = document.getElementById("sticks-2");
+var bondsButton2 = document.getElementById("bonds-2");
 
 // Get 3D elements from each marker
-var marker1AModel = document.getElementById("marker-1A-mol");
-var marker1BModel = document.getElementById("marker-1B-mol");
+var cartoon1A = document.querySelector("#cartoon1A");
+var sticks1A = document.querySelector("#sticks1A");
+var bonds1A = document.querySelector("#bonds1A");
+var cartoon1B = document.querySelector("#cartoon1B");
+var sticks1B = document.querySelector("#sticks1B");
+var bonds1B = document.querySelector("#bonds1B");
 
-var marker2AModel = document.getElementById("marker-2A-mol");
-var marker2BModel = document.getElementById("marker-2B-mol");
+var cartoon2A = document.querySelector("#cartoon2A");
+var sticks2A = document.querySelector("#sticks2A");
+var bonds2A = document.querySelector("#bonds2A");
+var cartoon2B = document.querySelector("#cartoon2B");
+var sticks2B = document.querySelector("#sticks2B");
+var bonds2B = document.querySelector("#bonds2B");
+
+var cartoon1 = [cartoon1A, cartoon1B];
+var sticks1 = [sticks1A, sticks1B];
+var bonds1 = [bonds1A, bonds1B];
+
+var cartoon2 = [cartoon2A, cartoon2B];
+var sticks2 = [sticks2A, sticks2B];
+var bonds2 = [bonds2A, bonds2B];
 
 var resetActivityButton = document.querySelector("reset-activity");
 
-// Store models in arrays for easier handling
-var modelsMarker1 = [marker1AModel, marker1BModel];
-var modelsMarker2 = [marker2AModel, marker2BModel];
-
-// Select first option by default
-marker1Select.selectedIndex = 0;
-marker2Select.selectedIndex = 0;
 
 // Reset both markers to default state
 function handleReset(e) {
-  marker1Select.selectedIndex = 0;
-  marker2Select.selectedIndex = 0;
+  cartoonButton1.classList.add("active");
+  sticksButton1.classList.add("active");
+  bondsButton1.classList.add("active");
+  cartoonButton2.classList.add("active");
+  sticksButton2.classList.add("active");
+  bondsButton2.classList.add("active");
 
-  // There's a bug on A-frame. We need to remove the
-  // attribute and add it again in order see the model change
-  marker1AModel.removeAttribute("gltf-model");
-  marker1BModel.removeAttribute("gltf-model");
+  bonds1A.setAttribute("visible", true);
+  bonds1B.setAttribute("visible", true);
+  sticks1A.setAttribute("visible", true);
+  sticks1B.setAttribute("visible", true);
+  cartoon1A.setAttribute("visible", true);
+  cartoon1B.setAttribute("visible", true);
 
-  marker2AModel.removeAttribute("gltf-model");
-  marker2BModel.removeAttribute("gltf-model");
-
-  marker1AModel.setAttribute("gltf-model", "#orb1");
-  marker1BModel.setAttribute("gltf-model", "#orb1");
-  marker2AModel.setAttribute("gltf-model", "#orb1");
-  marker2BModel.setAttribute("gltf-model", "#orb1");
-
-  marker1AModel.setAttribute("model-opacity", 0.5);
-  marker1BModel.setAttribute("model-opacity", 0.5);
-  marker2AModel.setAttribute("model-opacity", 0.5);
-  marker2BModel.setAttribute("model-opacity", 0.5);
+  bonds2A.setAttribute("visible", true);
+  bonds2B.setAttribute("visible", true);
+  sticks2A.setAttribute("visible", true);
+  sticks2B.setAttribute("visible", true);
+  cartoon2A.setAttribute("visible", true);
+  cartoon2B.setAttribute("visible", true);
 }
 
 function handleSelection(e) {
   e.preventDefault();
+  var id = e.target.id;
+  var marker = id.substring(id.length - 1);
+  var selection = id.substring(0, id.length - 2);
 
-  // Which marker was selected? Get its models & its marker elements
-  var markerSelected =
-    e.target.id === "select-marker-1" ? markers[0] : markers[1];
-  var modelsSelected =
-    e.target.id === "select-marker-1" ? modelsMarker1 : modelsMarker2;
-  var index = markerSelected.selectedIndex;
+  var isCartoonVisible, isSticksVisible, isBondsVisible;
 
-  // Select index starts with 0 and IDs starts with 1
-  // Be careful if using multiple option groups
-  var assetIndex = index + 1;
+  if (marker === "1") {
+    isCartoonVisible = cartoon1A.getAttribute("visible");
+    isSticksVisible = sticks1A.getAttribute("visible");
+    isBondsVisible = bonds1A.getAttribute("visible");
 
-  // There's a bug on A-frame. We need to remove the
-  // attribute and add it again in order see the model change
-  modelsSelected[0].removeAttribute("gltf-model");
-  modelsSelected[1].removeAttribute("gltf-model");
+    if (selection === "sticks") {
+      if (isSticksVisible) {
+        bonds1A.setAttribute("visible", false);
+        bonds1B.setAttribute("visible", false);
+        bondsButton1.classList.remove("active")
+      }
+      sticks1A.setAttribute("visible", !isSticksVisible);
+      sticks1B.setAttribute("visible", !isSticksVisible);
+      sticksButton1.classList.toggle("active");
+    } else if(selection === "cartoon") {
+      cartoon1A.setAttribute("visible", !isCartoonVisible);
+      cartoon1B.setAttribute("visible", !isCartoonVisible);
+      cartoonButton1.classList.toggle("active");
+    } else {
+      if (!isSticksVisible) {
+        bonds1A.setAttribute("visible", false);
+        bonds1B.setAttribute("visible", false);
+        bondsButton1.classList.remove("active");
+      } else {
+        bonds1A.setAttribute("visible", !isBondsVisible);
+        bonds1B.setAttribute("visible", !isBondsVisible);
+        bondsButton1.classList.toggle("active");
+      }
+    }
+  } else {
+    isCartoonVisible = cartoon2A.getAttribute("visible");
+    isSticksVisible = sticks2A.getAttribute("visible");
+    isBondsVisible = bonds2A.getAttribute("visible");
 
-  // Select the asset that corresponds to the option selected
-  modelsSelected[0].setAttribute("gltf-model", "#orb" + assetIndex);
-  modelsSelected[0].setAttribute("model-opacity", 0.5);
-  modelsSelected[1].setAttribute("gltf-model", "#orb" + assetIndex);
-  modelsSelected[1].setAttribute("model-opacity", 0.5);
+    if (selection === "sticks") {
+      if (isSticksVisible) {
+        bonds2A.setAttribute("visible", false);
+        bonds2B.setAttribute("visible", false);
+        bondsButton2.classList.remove("active")
+      }
+      sticks2A.setAttribute("visible", !isSticksVisible);
+      sticks2B.setAttribute("visible", !isSticksVisible);
+      sticksButton2.classList.toggle("active");
+    } else if(selection === "cartoon") {
+      cartoon2A.setAttribute("visible", !isCartoonVisible);
+      cartoon2B.setAttribute("visible", !isCartoonVisible);
+      cartoonButton2.classList.toggle("active");
+    } else {
+      if (!isSticksVisible) {
+        bonds2A.setAttribute("visible", false);
+        bonds2B.setAttribute("visible", false);
+        bondsButton2.classList.remove("active");
+      } else {
+        bonds2A.setAttribute("visible", !isBondsVisible);
+        bonds2B.setAttribute("visible", !isBondsVisible);
+        bondsButton2.classList.toggle("active");
+      }
+    }
+  }
+  
 }
 
-marker1Select.addEventListener("change", handleSelection);
-marker2Select.addEventListener("change", handleSelection);
+cartoonButton1.addEventListener("click", handleSelection);
+sticksButton1.addEventListener("click", handleSelection);
+bondsButton1.addEventListener("click", handleSelection);
+cartoonButton2.addEventListener("click", handleSelection);
+sticksButton2.addEventListener("click", handleSelection);
+bondsButton2.addEventListener("click", handleSelection);
 resetActivityButton.addEventListener("resetActivity", handleReset);
