8fa6bba10dbf7231fa72fff60418904f12d75788
Fabio Cortes <fcor14@hotmail.com>
2020-07-10 16:39:17 -0500

    Fixed first dock

--- modules/atomic-structures/xray-docking/index.js
+++ modules/atomic-structures/xray-docking/index.js
@@ -18,28 +18,36 @@ AFRAME.registerComponent("interactive-molecules", {
     this.markerFullProtA = this.el.sceneEl.querySelector("#marker-fullprotA");
     this.markerPept1A = this.el.sceneEl.querySelector("#marker-pept1A");
     this.markerPept2A = this.el.sceneEl.querySelector("#marker-pept2A");
+    this.r1Pept1A = this.el.sceneEl.querySelector("#r1-pept1A");
+    this.r2Pept1A = this.el.sceneEl.querySelector("#r2-pept1A");
 
     this.markerMeshB = this.el.sceneEl.querySelector("#marker-meshB");
     this.markerFullProtB = this.el.sceneEl.querySelector("#marker-fullprotB");
     this.markerPept1B = this.el.sceneEl.querySelector("#marker-pept1B");
     this.markerPept2B = this.el.sceneEl.querySelector("#marker-pept2B");
+    this.r1Pept1B = this.el.sceneEl.querySelector("#r1-pept1B");
+    this.r2Pept1B = this.el.sceneEl.querySelector("#r2-pept1B");
+
+    this.r1Pept1APosition = new THREE.Vector3();
+    this.r2Pept1APosition = new THREE.Vector3();
+    this.r1Pept1BPosition = new THREE.Vector3();
+    this.r2Pept1BPosition = new THREE.Vector3();
 
     // Global parameters: Targets, positions and quaternions
     // first piece
-    this.p0 = [1.24, -0.32, -10.0];
-    this.r0 = [0.7, 0.0, 0.0, 0.71];
+    this.r1Pept1 = [0.9, -0.5, -9.2];
+    this.r2Pept1 = [2.56, 0, -9];
 
     // second piece
     this.p1 = [-1.56, -0.28, -10.0];
     this.r1 = [0.7, 0.0, 0.0, 0.71];
 
     // tolerances
-    this.tol_r = 0.58;
     this.tol_d = 0.58;
 
     // define starting target position
-    this.tp = this.p0;
-    this.tr = this.r0;
+    this.ref1 = this.r1Pept1;
+    this.ref2 = this.r2Pept1;
     this.stage = 0;
 
     // Interval for running tick function - in ms
@@ -97,22 +105,34 @@ AFRAME.registerComponent("interactive-molecules", {
       return;
     }
 
+    var pept1ARefs = [this.r1Pept1APosition, this.r2Pept1APosition];
+    var pept1BRefs = [this.r1Pept1BPosition, this.r2Pept1BPosition];
+
+    this.el.sceneEl.object3D.updateMatrixWorld();
+
+    //  Get positions
+    this.r1Pept1APosition.setFromMatrixPosition(this.r1Pept1A.object3D.matrixWorld);
+    this.r1Pept1BPosition.setFromMatrixPosition(this.r1Pept1B.object3D.matrixWorld);
+    this.r2Pept1APosition.setFromMatrixPosition(this.r2Pept1A.object3D.matrixWorld);
+    this.r2Pept1BPosition.setFromMatrixPosition(this.r2Pept1B.object3D.matrixWorld);
+
     var markerSide = this.visibleMarkers.includes("watA") ? "A" : "B";
 
-    var marker = markerSide === "A" ? this.marker1A : this.marker1B;
+    var refs = markerSide === "A" ? pept1ARefs : pept1BRefs;
+  
+    var r1Pos = refs[0].toArray();
+    var r2Pos = refs[1].toArray();
 
-    marker3DObject = marker.object3D;
-
-    var mp = marker3DObject.position.toArray();
-    var mr = marker3DObject.quaternion.toArray();
-
-    // compute distance to reference
-    var d = norm(this.tp, mp, 3);
+    // compute distance to references
+    var d1 = norm(this.ref1, r1Pos, 3);
+    var d2 = norm(this.ref2, r2Pos, 3);
 
     // compute L2 distance of euler angles with reference
-    var r = norm(this.tr, mr, 4);
+    // var r = norm(this.tr, mr, 4);
 
-    if (r < this.tol_r && d < this.tol_d) {
+    console.log(d2);
+
+    if (d1 < this.tol_d && d2 < this.tol_d) {
       if (this.stage === 1) {
         // hide all fixed objects
         this.markerPept2A.setAttribute("visible", false);
@@ -139,11 +159,11 @@ AFRAME.registerComponent("interactive-molecules", {
         this.markerPept2B.setAttribute("visible", true);
 
         // change target
-        this.tp = this.p1;
-        this.tr = this.r1;
+        // this.tp = this.p1;
+        // this.tr = this.r1;
 
         // increment stage
-        this.stage += 1;
+        // this.stage += 1;
       }
     }
   },
