9f96695741045f3a16de425050bd3e6ed42ed0d2
Fabio Cortes <fcor14@hotmail.com>
2020-11-09 11:21:41 +0100

    Added basic funcionality and styles to policy banner

---
+++ js/handlePrivacyBanner.js
@@ -0,0 +1,18 @@
+var acceptBtn = document.getElementById("accept-privacy");
+var banner = document.getElementById("privacy");
+var expiresIn = 60000;
+
+function handleAccept(e) {
+  banner.classList.add("hidden");
+  var expire = new Date(Date.now() + expiresIn);
+  document.cookie = "policy=accepted; expires=" + expire + ";";
+}
+
+var hasUserAccepted = document.cookie.split(";").some(function (item) {
+  return item.trim().indexOf("policy=") == 0;
+});
+
+if (!hasUserAccepted) {
+  banner.classList.remove("hidden");
+  acceptBtn.addEventListener("click", handleAccept);
+}
