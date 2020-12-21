990b521e30debf22371aeddab3bc83810242cba6
Fabio Cortes <fcor14@hotmail.com>
2020-12-21 15:14:15 +0100

    Checkpoint...

--- modules/virtual-modeling-kit/3Dutils.js
+++ modules/virtual-modeling-kit/3Dutils.js
@@ -179,6 +179,7 @@ function createSticks(pdb) {
 
       var radius = 0.12;
 
+      // Bond rules for C 
       if (pdb.elements[atomIndex] === 5 && pdb.elements[bondedAtomIndex] === 5) {
         var atom1Bonds = allBonds[atom].length;
         var atom2Bonds = allBonds[bondedAtom].length;
@@ -193,6 +194,20 @@ function createSticks(pdb) {
 
         if (atom1Bonds === 2 && atom2Bonds === 2) {
           radius = 0.35;
+        }
+      }
+
+       // Bond rules for O
+      //  Si es O Y solo tiene un enlace!!!
+       if (pdb.elements[atomIndex] === 7) {
+        console.log("Oxigeno")
+        var atom1Bonds = allBonds[atom].length;
+        var atom2Bonds = allBonds[bondedAtom].length;
+
+        if (atom1Bonds === 1) {
+          radius = 0.25;
+        } else {
+          radius = 0.12;
         }
       }
 
