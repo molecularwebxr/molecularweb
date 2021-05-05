9cd7c110eeaca2a6eb011def4f7da6e23f51cbcf
Fabio Cortes <fcor14@hotmail.com>
2021-05-05 10:01:24 +0200

    Added new models on atomic orbitals

--- modules/orbitals-and-molecular-shapes/atomic-orbitals/index.js
+++ modules/orbitals-and-molecular-shapes/atomic-orbitals/index.js
@@ -5,8 +5,14 @@ var markers = [marker1Select, marker2Select];
 var marker1AModel = document.getElementById("marker-1A-mol");
 var marker1BModel = document.getElementById("marker-1B-mol");
 
+var marker1AOrb = document.getElementById("marker-1A-orb");
+var marker1BOrb = document.getElementById("marker-1B-orb");
+
 var marker2AModel = document.getElementById("marker-2A-mol");
 var marker2BModel = document.getElementById("marker-2B-mol");
+
+var marker2AOrb = document.getElementById("marker-2A-orb");
+var marker2BOrb = document.getElementById("marker-2B-orb");
 
 var resetActivityButton = document.querySelector("reset-activity");
 
@@ -27,8 +33,14 @@ function handleReset(e) {
 
   marker2AModel.removeAttribute("gltf-model");
   marker2BModel.removeAttribute("gltf-model");
+  marker2BModel.removeAttribute("gltf-model");
   marker2AModel.removeAttribute("obj-model");
   marker2AModel.removeAttribute("obj-model");
+
+  marker1AOrb.removeAttribute("gltf-model");
+  marker2AOrb.removeAttribute("gltf-model");
+  marker1BOrb.removeAttribute("gltf-model");
+  marker2BOrb.removeAttribute("gltf-model");
 
   marker1AModel.setAttribute("gltf-model", "#orb1");
   marker1BModel.setAttribute("gltf-model", "#orb1");
@@ -39,7 +51,6 @@ function handleReset(e) {
   marker1BModel.setAttribute("model-opacity", 0.5);
   marker2AModel.setAttribute("model-opacity", 0.5);
   marker2BModel.setAttribute("model-opacity", 0.5);
- 
 }
 
 function handleSelection(e) {
@@ -54,7 +65,7 @@ function handleSelection(e) {
   let assetIndex;
   if (index < 25) {
     assetIndex = index + 1;
-  } else if (index < 45) {
+  } else if (index < 50) {
     assetIndex = index + 2;
   } else {
     assetIndex = index + 3;
@@ -64,12 +75,50 @@ function handleSelection(e) {
   modelsSelected[0].removeAttribute("obj-model");
   modelsSelected[1].removeAttribute("gltf-model");
   modelsSelected[1].removeAttribute("obj-model");
-  if (index < 45) {
-    modelsSelected[0].setAttribute("gltf-model", "#orb" + assetIndex);
-    modelsSelected[0].setAttribute("model-opacity", 0.5);
-    modelsSelected[1].setAttribute("gltf-model", "#orb" + assetIndex);
-    modelsSelected[1].setAttribute("model-opacity", 0.5);
+  marker1AOrb.removeAttribute("gltf-model");
+  marker2AOrb.removeAttribute("gltf-model");
+  marker1BOrb.removeAttribute("gltf-model");
+  marker2BOrb.removeAttribute("gltf-model");
+  
+  if (index < 50) {
+    if (index > 24 && index < 30) {
+      modelsSelected[0].setAttribute("visible", false);
+      modelsSelected[1].setAttribute("visible", false);
+
+      if (e.target.id === "select-marker-1") {
+        marker1AOrb.setAttribute("visible", true);
+        marker1BOrb.setAttribute("visible", true);
+
+        marker2AOrb.setAttribute("visible", false);
+        marker2BOrb.setAttribute("visible", false);
+
+        marker1AOrb.setAttribute("gltf-model", "#orb" + assetIndex);
+        marker1BOrb.setAttribute("gltf-model", "#orb" + assetIndex);
+      } else {
+        marker2AOrb.setAttribute("visible", true);
+        marker2BOrb.setAttribute("visible", true);
+
+        marker1AOrb.setAttribute("visible", false);
+        marker1BOrb.setAttribute("visible", false);
+
+        marker2AOrb.setAttribute("gltf-model", "#orb" + assetIndex);
+        marker2BOrb.setAttribute("gltf-model", "#orb" + assetIndex);
+      }
+    } else {
+      marker1AOrb.setAttribute("visible", false);
+      marker1BOrb.setAttribute("visible", false);
+      marker2AOrb.setAttribute("visible", false);
+      marker2BOrb.setAttribute("visible", false);
+      modelsSelected[0].setAttribute("visible", true);
+      modelsSelected[1].setAttribute("visible", true);
+      modelsSelected[0].setAttribute("gltf-model", "#orb" + assetIndex);
+      modelsSelected[1].setAttribute("gltf-model", "#orb" + assetIndex);
+      modelsSelected[0].setAttribute("model-opacity", 0.5);
+      modelsSelected[1].setAttribute("model-opacity", 0.5);
+    }
   } else {
+    modelsSelected[0].setAttribute("visible", true);
+    modelsSelected[1].setAttribute("visible", true);
     modelsSelected[0].setAttribute(
       "obj-model",
       "obj:#orb" + assetIndex + "obj; mtl: #orb" + assetIndex + "mtl"
@@ -80,6 +129,8 @@ function handleSelection(e) {
       "obj:#orb" + assetIndex + "obj; mtl: #orb" + assetIndex + "mtl"
     );
     modelsSelected[1].setAttribute("model-opacity", 1);
+    modelsSelected[0].setAttribute("scale", { x: 0.2, y: 0.2, z: 0.2 });
+    modelsSelected[1].setAttribute("scale", { x: 0.2, y: 0.2, z: 0.2 });
   }
 }
 
