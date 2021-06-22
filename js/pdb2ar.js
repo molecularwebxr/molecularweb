233b7776434a80f40efa2ceef6f17bf1a2d4889f
Fabio Cortes <fcor14@hotmail.com>
2021-06-22 12:07:51 +0200

    Added individual atom handling and improved TCL algorithm

--- js/pdb2ar.js
+++ js/pdb2ar.js
@@ -35,80 +35,110 @@ function readPdb(e) {
 
   var lines = pdbString.split("\n");
 
-  for (i = 0; i < lines.length; i++) {
-    if ( lines[i].substring(0, 4) === "ATOM" || lines[i].substring(0, 6) === "HETATM") {
-      var thisChain = lines[i].substring(21, 22).trim().toUpperCase();
-      var chainIsThere = -1;
-      var thisRes = lines[i].substring(17, 20).trim().toUpperCase();
-      var thisResNo = parseInt(lines[i].substring(23, 26).trim().toUpperCase());
-      var thisEl = lines[i].substring(76, 78).trim().toUpperCase();
-      var thisElNum = lines[i].substring(7, 11).trim();
+  var atoms = lines.filter(function (line, index) {
+    return line.substring(0, 4) === "ATOM" || line.substring(0, 6) === "HETATM"
+  });
 
-      for (j = 0; j < chains.length; j++) {
-        if (thisChain === chains[j]) {
-          chainIsThere = j;
-          break;
-        }
+  atoms.forEach(function (atom, index) {
+    var thisChain = atom.substring(21, 22).trim().toUpperCase();
+    var chainIsThere = -1;
+    var thisRes = atom.substring(17, 20).trim().toUpperCase();
+    var thisResNo = parseInt(atom.substring(23, 26).trim().toUpperCase());
+    var thisEl = atom.substring(76, 78).trim().toUpperCase();
+    var thisElNum = atom.substring(7, 11).trim();
+    var pdbIndex = index;
+
+    for (j = 0; j < chains.length; j++) {
+      if (thisChain === chains[j]) {
+        chainIsThere = j;
+        break;
       }
-      if (chainIsThere > -1) {
-        chainstrings[chainIsThere] = chainstrings[chainIsThere] + "\n" + lines[i];
-      } else {
-        chains.push(thisChain);
-        chainstrings.push(lines[i]);
-        if ("ALA|CYS|ASP|GLU|PHE|GLY|HIS|ILE|LYS|LEU|MET|ASN|PRO|GLN|ARG|SER|THR|VAL|TRP|TYR|MSE|HSE|HSD|HID|HIE".match(thisRes)) {
-          chaintypes.push("protein");
-        } else if ("DA|DC|DG|DT|A|C|G|U".match(thisRes)) {
-          chaintypes.push("nucleic");
-        } else {
-          chaintypes.push("other");
-        }
-      }
-
-      if (!"ALA|CYS|ASP|GLU|PHE|GLY|HIS|ILE|LYS|LEU|MET|ASN|PRO|GLN|ARG|SER|THR|VAL|TRP|TYR|MSE|HSE|HSD|HID|HIE|DA|DC|DG|DT|A|C|G|U|".match(thisRes)){
-        if (thisRes === "WAT" || thisRes === "HOH" || thisRes === "TIP") {
-          if (!isWaterChecked) {
-            ligandchains.push(thisChain);
-            ligandresnames.push(thisRes);
-            ligandresnos.push(thisResNo);
-            ligandComments.push(" ");
-          }
-        } else {
-          var isChainAdded = ligandchains.includes(thisChain);
-          var isResAdded = ligandresnames.includes(thisRes);
-          var isResNoAdded = ligandresnos.includes(thisResNo);
-
-          if (!(isChainAdded && isResAdded && isResNoAdded)) {
-            ligandchains.push(thisChain);
-            ligandresnames.push(thisRes);
-            ligandresnos.push(thisResNo);
-            ligandComments.push(" ");
-          }
-        }
-      }
-
-      if (thisEl !== "C" && thisEl !== "H" && thisEl !== "O" && thisEl !== "N" && thisEl !== "S" && thisEl !== "P") {
-        ligandchains.push(thisChain);
-        ligandresnames.push(thisRes);
-        ligandresnos.push(thisResNo);
-        ligandComments.push(thisEl + " atom " + thisElNum);
-      }
-
     }
-  }
+    if (chainIsThere > -1) {
+      chainstrings[chainIsThere] = chainstrings[chainIsThere] + "\n" + atom;
+    } else {
+      chains.push(thisChain);
+      chainstrings.push(atom);
+      if ("ALA|CYS|ASP|GLU|PHE|GLY|HIS|ILE|LYS|LEU|MET|ASN|PRO|GLN|ARG|SER|THR|VAL|TRP|TYR|MSE|HSE|HSD|HID|HIE".match(thisRes)) {
+        chaintypes.push("protein");
+      } else if ("DA|DC|DG|DT|A|C|G|U".match(thisRes)) {
+        chaintypes.push("nucleic");
+      } else {
+        chaintypes.push("other");
+      }
+    }
+
+    if (!"ALA|CYS|ASP|GLU|PHE|GLY|HIS|ILE|LYS|LEU|MET|ASN|PRO|GLN|ARG|SER|THR|VAL|TRP|TYR|MSE|HSE|HSD|HID|HIE|DA|DC|DG|DT|A|C|G|U|".match(thisRes)){
+      if (thisRes === "WAT" || thisRes === "HOH" || thisRes === "TIP") {
+        if (!isWaterChecked) {
+          ligandchains.push(thisChain);
+          ligandresnames.push(thisRes);
+          ligandresnos.push(thisResNo);
+          ligandComments.push(" ");
+        }
+      } else {
+        var isChainAdded = ligandchains.includes(thisChain);
+        var isResAdded = ligandresnames.includes(thisRes);
+        var isResNoAdded = ligandresnos.includes(thisResNo);
+
+        if (!(isChainAdded && isResAdded && isResNoAdded)) {
+          ligandchains.push(thisChain);
+          ligandresnames.push(thisRes);
+          ligandresnos.push(thisResNo);
+          ligandComments.push({
+            display: false,
+            element: "",
+            number: "",
+            index: ""
+          });
+        }
+      }
+    }
+
+    if (thisEl !== "C" && thisEl !== "H" && thisEl !== "O" && thisEl !== "N" && thisEl !== "S" && thisEl !== "P") {
+      ligandchains.push(thisChain);
+      ligandresnames.push(thisRes);
+      ligandresnos.push(thisResNo);
+      ligandComments.push({
+        display: true,
+        element: thisEl,
+        number: thisElNum,
+        index: pdbIndex
+      });
+    }
+  })
+
 
   if (isWaterChecked) {
     ligandchains.push(" ");
     ligandresnames.push("HOH");
     ligandresnos.push(" ");
-    ligandComments.push(" ");
+    ligandComments.push({
+      display: false,
+      element: "",
+      number: "",
+      index: ""
+    });
+
     ligandchains.push(" ");
     ligandresnames.push("WAT");
     ligandresnos.push(" ");
-    ligandComments.push(" ");
+    ligandComments.push({
+      display: false,
+      element: "",
+      number: "",
+      index: ""
+    });
+
     ligandchains.push(" ");
     ligandresnames.push("TIP");
     ligandresnos.push(" ");
-    ligandComments.push(" ");
+    ligandComments.push({
+      display: false,
+      element: "",
+      number: "",
+      index: ""
+    });
   }
 
   ligandGrid.classList.remove("hidden");
@@ -141,6 +171,7 @@ function readPdb(e) {
   });
 
   ligandchains.forEach(function (ligandChain, index) {
+    var commentString = `${ligandComments[index].element} atom ${ligandComments[index].number}`
     var rowString = /* html */ `
     <!-- Grid row -->
     <div class="grid-element">
@@ -163,7 +194,7 @@ function readPdb(e) {
       </select>
     </div>
     <div class="grid-element">
-      <p class="grid-text">${ligandComments[index]}</p>
+      <p class="grid-text">${ligandComments[index].display ? commentString : " "}</p>
     </div>
     `;
     ligandGrid.insertAdjacentHTML("beforeend", rowString);
@@ -182,91 +213,66 @@ function buildVmd(e) {
     var value = document.getElementById("proteinRow-" + index).value;
     var color = document.getElementById("proteinRowColor-" + index).value;
 
+    if (value === "No") {
+      return;
+    }
+
+    tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
+
     if (value === "NewCartoon (cartoons)") {
-      tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
       tclString += "mol modstyle " + nout + " 0 NewCartoon 0.300000 10.000000 4.100000 0\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
     if (value === "Licorice (sticks)") {
-      tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
       tclString += "mol modstyle " + nout + " 0 Licorice 0.300000 12.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
     if (value === "VDW (spheres)" || value === "Surf (surface)") {
-      tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
       tclString += "mol modstyle " + nout + " 0 VDW 1.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
     if (value === "Tube") {
-      tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
       tclString += "mol modstyle " + nout + " 0 Tube 0.300000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
     if (value === "CPK") {
-      tclString += "mol modselect " + nout + " 0 chain " + chain + "\n";
       tclString += "mol modstyle " + nout + " 0 CPK 1.000000 0.300000 12.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
-
-
-    // if (value === "Surf (surface)") {
-    //   tclString += "mol modselect " + nout + " 0 chain " + chain + " and resname \n";
-    //   tclString += "mol modstyle " + nout + " 0 Surf 1.400000 0.000000\n";
-    //   tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-    //   tclString += "\nmol addrep 0\n";
-    //   nout++;
-    // }
+    tclString += "mol modcolor " + nout + " 0 " + color + "\n";
+    tclString += "\nmol addrep 0\n";
+    nout++;
   });
 
   ligandchains.forEach(function (ligandChain, index) {
     var value = document.getElementById("ligandRow-" + index).value;
     var color = document.getElementById("ligandRowColor-" + index).value;
 
-    if (value == "Licorice (sticks)") {
+    if (value === "No") {
+      return;
+    }
+
+    if (ligandComments[index].display) {
+      tclString += "mol modselect " + nout + " 0 chain " + ligandChain + " and index " + ligandComments[index].index + "\n";
+    } else {
       tclString += "mol modselect " + nout + " 0 chain " + ligandChain + " and resname " + ligandresnames[index] + " and resid " + ligandresnos[index] + "\n";
+    }
+
+    if (value === "Licorice (sticks)") {
       tclString += "mol modstyle " + nout + " 0 Licorice 0.300000 12.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
-    if (value == "VDW (spheres)" || value === "Surf (surface)") {
-      tclString += "mol modselect " + nout + " 0 chain " + ligandChain + " and resname " + ligandresnames[index] + " and resid " + ligandresnos[index] + "\n";
+    if (value === "VDW (spheres)" || value === "Surf (surface)") {
       tclString += "mol modstyle " + nout + " 0 VDW 1.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
-    if (value == "CPK") {
-      tclString += "mol modselect " + nout + " 0 chain " + ligandChain + " and resname " + ligandresnames[index] + " and resid " + ligandresnos[index] + "\n";
+    if (value === "CPK") {
       tclString += "mol modstyle " + nout + " 0 CPK 1.000000 0.300000 12.000000 12.000000\n";
-      tclString += "mol modcolor " + nout + " 0 " + color + "\n";
-      tclString += "\nmol addrep 0\n";
-      nout++;
     }
 
-    // if (value == "Surf (surface)") {
-    //     tclString += "mol modselect " + nout + " 0 chain " + ligandChain + " and resname " + ligandresnames[index] + " and resid " + ligandresnos[index] + "\n"
-    //     tclString += "mol modstyle " + nout + " 0 Surf 1.400000 0.000000\n"
-    //     tclString += "mol modcolor " + nout + " 0 " + color + "\n"
-    //     tclString += "\nmol addrep 0\n"
-    //     nout++
-    //   }
+    tclString += "mol modcolor " + nout + " 0 " + color + "\n";
+    tclString += "\nmol addrep 0\n";
+    nout++;
+
   });
 
   tclString += endTcl;
@@ -297,8 +303,6 @@ function handleSubmit(e) {
     title,
     email,
   };
-
-  console.log(data.pdb)
 
   fetch("https://molecularweb.epfl.ch/backend/api/pdb2ar/pdb", {
     method: "POST",
