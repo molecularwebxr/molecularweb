26077275fa570136ddd2c3e97db8ff7eab01140d
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-03-22 13:16:54 +0600

    Update translation.js

--- locales/kk/translation.js
+++ locales/kk/translation.js
@@ -1,1 +1,9 @@
-
+{
+    "menu": {
+      "about": "About",
+      "getInTouch": "Contact",
+      "feedback": "Feedback",
+      "language": "Language",
+      "instructions": "Instructions",
+      "markers": "Markers"
+    },
